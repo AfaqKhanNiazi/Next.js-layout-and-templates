082296ae933016ae75cfede975d217ed63a76f54
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-02-01 11:52:05 +0500

    dashboard not change

--- src/app/(auth)/forget-password/page.js
+++ src/app/(auth)/forget-password/page.js
@@ -2,7 +2,7 @@
 export default function ForgetPassword() {
     return(
         <div className="container mx-auto text-center p-20">
-     <h1 className="font-bold text-5xl">Froget-Password</h1>
+     <h1 className="font-bold text-5xl">Forget-Password</h1>
         </div>
     )
 }
